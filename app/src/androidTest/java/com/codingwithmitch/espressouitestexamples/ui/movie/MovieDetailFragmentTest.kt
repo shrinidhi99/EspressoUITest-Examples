f6c43a627a9f81e3b2eb33d02ee8c2b23cb9ef2c
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-12-04 16:31:02 +0530

    Fragments in Isolation Espresso Testing

---
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/movie/MovieDetailFragmentTest.kt
@@ -0,0 +1,37 @@
+package com.codingwithmitch.espressouitestexamples.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.codingwithmitch.espressouitestexamples.R
+import com.codingwithmitch.espressouitestexamples.data.DummyMovies.THE_RUNDOWN
+import com.codingwithmitch.espressouitestexamples.factory.MovieFragmentFactory
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MovieDetailFragmentTest {
+
+    @Test
+    fun test_isMovieDataVisible() {
+
+        // SETUP
+        val movie = THE_RUNDOWN
+        val fragmentFactory = MovieFragmentFactory()
+        val bundle = Bundle()
+        bundle.putInt("movie_id", movie.id)
+
+        val scenario = launchFragmentInContainer<MovieDetailFragment>(
+            fragmentArgs = bundle,
+            factory = fragmentFactory
+        )
+
+        onView(withId(R.id.movie_title)).check(matches(withText(movie.title)))
+
+        onView(withId(R.id.movie_description)).check(matches(withText(movie.description)))
+    }
+}
